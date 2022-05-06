42028069c983302ebd5943a45bba5285e5ae426b
siddhuWooqer <siddhant.jaiswal@wooqer.com>
2022-05-06 10:43:41 +0530

    Initial Commit

---
+++ ToasterLibrary/src/main/java/siddhu690/com/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,13 @@
+package siddhu690.com.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/siddhu690/com/toasterexample/MainActivity.java
@@ -0,0 +1,14 @@
+package siddhu690.com.toasterexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
